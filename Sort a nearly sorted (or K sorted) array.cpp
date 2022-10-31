48cc30151f5625ac80503053a09a688c19a50968
Mr.Incredible.29 <65172813+Naitik-Jain@users.noreply.github.com>
2022-10-31 21:24:57 +0530

    Update Sort a nearly sorted (or K sorted) array.cpp

--- Sort a nearly sorted (or K sorted) array.cpp
+++ Sort a nearly sorted (or K sorted) array.cpp
@@ -1,4 +1,3 @@
-/* Function to sort an array using insertion sort*/
 void insertionSort(int A[], int size)
 {
 int i, key, j;
@@ -6,11 +5,6 @@ for(i = 1; i < size; i++)
 {
 	key = A[i];
 	j = i - 1;
-
-	/* Move elements of A[0..i-1], that are
-		greater than key, to one
-		position ahead of their current position.
-		This loop will run at most k times */
 	while (j >= 0 && A[j] > key)
 	{
 		A[j + 1] = A[j];
@@ -20,5 +14,4 @@ for(i = 1; i < size; i++)
 }
 }
 
-// This code is contributed by Shivani
 
