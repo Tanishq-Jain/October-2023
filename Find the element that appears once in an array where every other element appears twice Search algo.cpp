88214e2e6d6a6dc9a29d02b9c31c3e91bc781ae8
Mr.Incredible.29 <65172813+Naitik-Jain@users.noreply.github.com>
2022-10-31 21:07:48 +0530

    Update Find the element that appears once in an array where every other element appears twice Search algo.cpp

--- Find the element that appears once in an array where every other element appears twice Search algo.cpp
+++ Find the element that appears once in an array where every other element appears twice Search algo.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 using namespace std;
 
+int findSingle(int A[], int ar_size)
 {
 	for (int i = 0; i < ar_size; i++) {
 		int count = 0;
+
 		for (int j = 0; j < ar_size; j++) {
 			if (A[i] == A[j]) {
 				count++;
@@ -19,6 +21,8 @@ int main()
 {
 	int ar[] = { 2, 3, 5, 4, 5, 3, 4 };
 	int n = sizeof(ar) / sizeof(ar[0]);
+
+	// Function call
 	cout << "Element occurring once is "
 		<< findSingle(ar, n);
 
