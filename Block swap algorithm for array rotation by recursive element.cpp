9e467fbbc32459ea97a260ee6ea407fd9584ee6d
Tanishq-Jain <73748532+Tanishq-Jain@users.noreply.github.com>
2022-10-31 20:06:43 +0530

    Update Block swap algorithm for array rotation by recursive element.cpp

--- Block swap algorithm for array rotation by recursive element.cpp
+++ Block swap algorithm for array rotation by recursive element.cpp
@@ -1,52 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-/*Prototype for utility functions */
+
 void printArray(int arr[], int size);
 void swap(int arr[], int fi, int si, int d);
 
 void leftRotate(int arr[], int d, int n)
 {
-	/* Return If number of elements to be rotated
+	
 	is zero or equal to array size */
 	if(d == 0 || d == n)
 		return;
-	/* If number of elements to be rotated is more than array size*/
+
 	if(d > n)
 	d = d % n;
-	/*If number of elements to be rotated
-	is exactly half of array size */
+
 	if(n - d == d)
 	{
 		swap(arr, 0, n - d, d);
 		return;
 	}
 		
-	/* If A is shorter*/	
+	
 	if(d < n - d)
 	{
 		swap(arr, 0, n - d, d);
 		leftRotate(arr, d, n - d);	
 	}
-	else /* If B is shorter*/	
+	else 	
 	{
 		swap(arr, 0, d, n - d);
 		leftRotate(arr + n - d, 2 * d - n, d); /*This is tricky*/
 	}
 }
 
-/*UTILITY FUNCTIONS*/
-/* function to print an array */
-void printArray(int arr[], int size)
-{
-	int i;
-	for(i = 0; i < size; i++)
-		cout << arr[i] << " ";
-	cout << endl;
-}
-
-/*This function swaps d elements starting at index fi
-with d elements starting at index si */
 void swap(int arr[], int fi, int si, int d)
 {
 	int i, temp;
@@ -58,7 +45,7 @@ void swap(int arr[], int fi, int si, int d)
 	}
 }
 
-// Driver Code
+
 int main()
 {
 	int arr[] = {1, 2, 3, 4, 5, 6, 7};
@@ -67,5 +54,5 @@ int main()
 	return 0;
 }
 
-// This code is contributed by rathbhupendra
+
 
