338373e998b9f3ce0080c5d62685a56b8401b812
Mr.Incredible.29 <65172813+Naitik-Jain@users.noreply.github.com>
2022-10-31 20:30:19 +0530

    Update Find a peak element which is not smaller than its neighbours by search algo.cpp
    Add a If condition in peak function

--- Find a peak element which is not smaller than its neighbours by search algo.cpp
+++ Find a peak element which is not smaller than its neighbours by search algo.cpp
@@ -4,6 +4,15 @@ using namespace std;
 
 int findPeak(int arr[], int n)
 {
+	
+	if (n == 1)
+		return 0;
+	if (arr[0] >= arr[1])
+		return 0;
+	if (arr[n - 1] >= arr[n - 2])
+		return n - 1;
+
+	
 	for (int i = 1; i < n - 1; i++) {
 
 		
